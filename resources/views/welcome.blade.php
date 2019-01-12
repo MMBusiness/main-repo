bf934e60596dfb9bf981f2043a793d9b8369fcba
Mikro Mika <dev.mikromika@gmail.com>
2019-01-12 20:18:28 +0200

    dev updatre

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel DEV environment
                 </div>
 
                 <div class="links">
